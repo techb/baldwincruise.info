2a45d79673976fe30d159652eb62f475a60bd38c
kbcarte <kbcarte@gmail.com>
2020-02-20 18:46:22 -0500

    fixed annoying js error, jQuery loads first now

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 console.log("https://github.com/techb");
 
 
+
 // https://github.com/objectivehtml/FlipClock
 var date = new Date(2020, 7, 21);
 var now = new Date();
 var diff = (date.getTime()/1000) - (now.getTime()/1000);
-var clock = jQuery('#timer-container').FlipClock(diff, {
+var clock = $('#timer-container').FlipClock(diff, {
 	clockFace: 'DailyCounter',
 	countdown: true,
 });
