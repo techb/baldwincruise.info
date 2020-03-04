9f5679721df5680896a55482ea90cd7ea489ac7b
kbcarte <kbcarte@gmail.com>
2020-03-03 19:43:57 -0500

    I'm happy with it, time to push live

--- js/scripts.js
+++ js/scripts.js
@@ -13,16 +13,22 @@ var clock = $('#timer-container').FlipClock(diff, {
 
 
 // https://codepen.io/mxbck/pen/xdaGNL
-const nav = document.querySelector('#nav');
-const menu = document.querySelector('#menu');
-const menuToggle = document.querySelector('.nav__toggle');
-let isMenuOpen = false;
-// TOGGLE MENU ACTIVE STATE
-menuToggle.addEventListener('click', e => {
-    e.preventDefault();
-    isMenuOpen = !isMenuOpen;
-    // toggle a11y attributes and active class
-    menuToggle.setAttribute('aria-expanded', String(isMenuOpen));
-    menu.hidden = !isMenuOpen;
-    nav.classList.toggle('nav--open');
+$('.menuBtn').click(function() {
+    $(this).toggleClass('act');
+        if($(this).hasClass('act')) {
+            $('.mainMenu').addClass('act');
+        }
+        else {
+            $('.mainMenu').removeClass('act');
+        }
+});
+
+$('.top-menu ul li a').click(function() {
+    $('.menuBtn').toggleClass('act');
+    if($(this).hasClass('act')) {
+        $('.mainMenu').addClass('act');
+    }
+    else {
+        $('.mainMenu').removeClass('act');
+    }
 });
