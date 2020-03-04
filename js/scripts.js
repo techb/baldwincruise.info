797a3919c827cd93ae1ef6ca0f48a72f218300d8
kbcarte <kbcarte@gmail.com>
2020-03-03 19:11:29 -0500

    mobile nav done, almost ready to go live

--- js/scripts.js
+++ js/scripts.js
@@ -10,3 +10,19 @@ var clock = $('#timer-container').FlipClock(diff, {
 	clockFace: 'DailyCounter',
 	countdown: true,
 });
+
+
+// https://codepen.io/mxbck/pen/xdaGNL
+const nav = document.querySelector('#nav');
+const menu = document.querySelector('#menu');
+const menuToggle = document.querySelector('.nav__toggle');
+let isMenuOpen = false;
+// TOGGLE MENU ACTIVE STATE
+menuToggle.addEventListener('click', e => {
+    e.preventDefault();
+    isMenuOpen = !isMenuOpen;
+    // toggle a11y attributes and active class
+    menuToggle.setAttribute('aria-expanded', String(isMenuOpen));
+    menu.hidden = !isMenuOpen;
+    nav.classList.toggle('nav--open');
+});
